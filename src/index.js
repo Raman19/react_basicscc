0c5748d48fd99e32dc7f66c1fcc0a6a941187e82
anish-kr07 <anishhit2016@gmail.com>
2017-01-10 08:41:38 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Basic from './Basic';
+import './index.css';
+import Sum from './Sum';
+import Counter from './Counter';
+import Timer from './Timer';
+import Boxes from './Boxes.js';
+import RandomBox  from './RandomBox';
+
+ReactDOM.render(
+  <div>
+ <RandomBox/>
+  </div>,
+  document.getElementById('root')
+);
